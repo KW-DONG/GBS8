4b71bca1d01f677923a27896462c72209fd95f46
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2020-10-08 16:07:02 +0800

    2020-10-8

--- gbs8/inc/gbs8_timer.h
+++ gbs8/inc/gbs8_timer.h
@@ -5,24 +5,41 @@
 #include <xc.h>
 #include <stdint.h>
 
-enum 
+
+enum TIM0_CS    //TMR0 Clock Source Select bit
 {
+    //when used as a timer, the Timer0 module will increment every instruction cycle (without prescaler)
     TIMER_MODE,
-    COUNTER_MODE,
+
+    //when used as a counter, the Timer0 module will increment on every rising or falling edge of the T0CKI pin
+    COUNTER_MODE
+};
+
+enum TIM0_SE    //TMR0 Source Edge Select bit
+{
+    L2H,        //Increment on low-to-high transition on T0CKI Pin
+    H2L         //Increment on high-to-low transition on T0CKI Pin
+};
+
+enum TIM0_PSA
+{
+    TIMER,
+    WDT
 };
 
 /**
  * @brief Timer0 configuration
- * @param mode
- * @note when used as a timer, the Timer0 module will increment every instruction cycle (without prescaler)
- *      when used as a counter, the Timer0 module will increment on every rising or falling edge of the T0CKI pin
+ * @param clockSource   timer0 can be either timer or counter
+ * @param sourceEdge    high to low or low to high
+ * @param assignment    Timer or watchdog
+ * @param prescaler     3-bits prescaler          
  */
-void GBS_Timer0_Mode(uint8_t mode);
+void GBS_Timer0_Config(uint8_t clockSoure, uint8_t sourceEdge, uint8_t assignment, uint8_t prescaler);
 
 //timer 1
-void GBS_Timer1_Mode(uint8_t mode);
+void GBS_Timer1_Config(uint8_t mode);
 
 //timer 2
-void GBS_Timer2_Mode(uint8_t mode);
+void GBS_Timer2_Config(uint8_t mode);
 
 #endif
--- gbs8/scr/gbs8_timer.c
+++ gbs8/scr/gbs8_timer.c
@@ -1,13 +1,11 @@
 #include "gbs8_timer.h"
 
-void GBS_Timer0_Mode(uint8_t mode)
+void GBS_Timer0_Config(uint8_t clockSoure, uint8_t sourceEdge, uint8_t assignment, uint8_t prescaler);
 {
-    PSA = 0;
-    switch (mode)
-    {
-        case TIMER_MODE:    T0CS = 0;
-        case COUNTER_MODE:  T0CS = 1;
-    }
+    T0CS = clockSource;
+    T0SE = sourceEdge;
+    PSA = assignment;
+    PS = prescaler;
 }
 
 void GBS_Timer1_Mode(uint8_t mode);
